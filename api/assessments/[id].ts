5c5eebedb52c715cf86d4542800d90eeaa9e9334
Sherry Zhang <sherryzhang22106@gmail.com>
2025-12-23 20:15:00 +0800

    Add CORS headers to assessment detail API

--- api/assessments/[id].ts
+++ api/assessments/[id].ts
@@ -11,6 +11,15 @@ function getPrisma() {
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
